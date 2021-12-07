6f1cd1240730a7c8307161424e2bd37025191882
Wintrmvte <redcode.labs@gmail.com>
2021-12-07 21:10:10 +0100

    Fixed payload name

--- payloads/random_confirm.py
+++ payloads/random_confirm.py
@@ -3,4 +3,4 @@ import random
 name="random_confirm"
 description="Chooses a random one-liner 'confirm()' payload. An alternative to standard 'alert()'"
 options = [[]] 
-payload = random.choice(open('random__confirm_payloads.txt').readlines())
+payload = random.choice(open('random_confirm_payloads.txt').readlines())
